bc2eb57c44ec1cf68a777bc286534624a29e3294
Michael Budden <bonjour@immbudden.com>
2019-01-24 20:16:24 -0800

    Contact page complete - buttons linked

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,3 @@
-const createPaginatedPages = require('gatsby-paginate')
 const path = require('path')
 
 // Implement the Gatsby API “createPages”. This is called once the
@@ -28,17 +27,6 @@ exports.createPages = ({ graphql, actions }) => {
           }
           console.log(result)
           // Create pages for each markdown file.
-
-          createPaginatedPages({
-            edges: result.data.posts.edges,
-            createPage: createPage,
-            pageTemplate: 'src/templates/news-story.js',
-            pageLength: 20,
-            pathPrefix: 'news',
-            buildPath: (index, pathPrefix) =>
-              index > 1 ? `${pathPrefix}/${index}` : `/${pathPrefix}`, // This is optional and this is the default
-          })
-
           result.data.allPrismicNewsStory.edges.forEach(({ node }) => {
             const path = node.uid
             createPage({
