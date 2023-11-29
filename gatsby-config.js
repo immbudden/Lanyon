a251fc021bd23677332d4354a86cac65fbdb288d
immbudden <bonjour@immbudden.com>
2023-11-29 00:27:10 +0000

    Update to include schema

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,59 +1,55 @@
 module.exports = {
-
-    siteMetadata: {
-        title: `Lanyon Group - A Belfast based Communications and PR agency`,
-        siteUrl: `https://lanyongroup.com`
-      },
+  siteMetadata: {
+    title: `Lanyon Group - A Belfast based Communications and PR agency`,
+    siteUrl: `https://lanyongroup.com`,
+  },
 
   plugins: [
     {
       resolve: `gatsby-plugin-sitemap`,
       options: {
         output: `/sitemap.xml`,
-      }
+      },
     },
     {
-        resolve: 'gatsby-plugin-robots-txt',
-        options: {
-        host: 'https://lanyongroup.com',
-        sitemap: 'https://lanyongroup.com/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://lanyongroup.com",
+        sitemap: "https://lanyongroup.com/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
     },
     {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          name: `images`,
-          path: `${__dirname}/src/images`,
-        },
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
     },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-emotion`,
-    'gatsby-plugin-react-helmet',
-    
+    "gatsby-plugin-react-helmet",
 
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [
-          `gatsby-remark-responsive-iframe`,
-        ],
+        plugins: [`gatsby-remark-responsive-iframe`],
       },
     },
 
-    `gatsby-transformer-sharp`, 
+    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    
+
     {
       resolve: "gatsby-plugin-netlify-cache",
       options: {
-        cachePublic: true
-      }
-    },    
+        cachePublic: true,
+      },
+    },
 
     {
-        resolve: `gatsby-plugin-typography`,
-        options: {
+      resolve: `gatsby-plugin-typography`,
+      options: {
         pathToConfigModule: `src/utils/typography.js`,
       },
     },
@@ -61,9 +57,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-136265004-1',
-        cookieDomain: 'lanyongroup.com'
-      }
+        trackingId: "UA-136265004-1",
+        cookieDomain: "lanyongroup.com",
+      },
     },
 
     {
@@ -74,21 +70,22 @@ module.exports = {
         allExtensions: true, // defaults to false
       },
     },
-    
+
     {
-      resolve: 'gatsby-source-prismic',
+      resolve: "gatsby-source-prismic",
       options: {
         // The name of your prismic.io repository. This is required.
         // Example: 'gatsby-source-prismic-test-site' if your prismic.io address
         // is 'gatsby-source-prismic-test-site.prismic.io'.
-        repositoryName: 'lanyon',
-  
+        repositoryName: "lanyon",
+
         // An API access token to your prismic.io repository. This is required.
         // You can generate an access token in the "API & Security" section of
         // your repository settings. Setting a "Callback URL" is not necessary.
         // The token will be listed under "Permanent access tokens".
-        accessToken: 'MC5YQzAxQVJBQUFETUFVNmd4.Q--_ve-_ve-_vSYF77-9JO-_vTzvv71b77-9Te-_ve-_ve-_ve-_ve-_vRcQUQnvv70zPe-_vQLvv70877-9Mg',
-  
+        accessToken:
+          "MC5YQzAxQVJBQUFETUFVNmd4.Q--_ve-_ve-_vSYF77-9JO-_vTzvv71b77-9Te-_ve-_ve-_ve-_ve-_vRcQUQnvv70zPe-_vQLvv70877-9Mg",
+
         // Set a link resolver function used to process links in your content.
         // Fields with rich text formatting or links to internal content use this
         // function to generate the correct link URL.
@@ -96,17 +93,19 @@ module.exports = {
         // provided to the function, as seen below. This allows you to use
         // different link resolver logic for each field if necessary.
         // See: https://prismic.io/docs/javascript/query-the-api/link-resolving
-        linkResolver: ({ node, key, value }) => doc => {
-          // Your link resolver
-        },
-  
+        linkResolver:
+          ({ node, key, value }) =>
+          (doc) => {
+            // Your link resolver
+          },
+
         // Set a list of links to fetch and be made available in your link
         // resolver function.
         // See: https://prismic.io/docs/javascript/query-the-api/fetch-linked-document-fields
         fetchLinks: [
           // Your list of links
         ],
-  
+
         // Set an HTML serializer function used to process formatted content.
         // Fields with rich text formatting use this function to generate the
         // correct HTML.
@@ -114,20 +113,17 @@ module.exports = {
         // provided to the function, as seen below. This allows you to use
         // different HTML serializer logic for each field if necessary.
         // See: https://prismic.io/docs/nodejs/beyond-the-api/html-serializer
-        htmlSerializer: ({ node, key, value }) => (
-          type,
-          element,
-          content,
-          children,
-        ) => {
-          // Your HTML serializer
-        },
-  
+        htmlSerializer:
+          ({ node, key, value }) =>
+          (type, element, content, children) => {
+            // Your HTML serializer
+          },
+
         // Set a default language when fetching documents. The default value is
         // '*' which will fetch all languages.
         // See: https://prismic.io/docs/javascript/query-the-api/query-by-language
-        lang: '*',
-  
+        lang: "*",
+
         // Set a function to determine if images are downloaded locally and made
         // available for gatsby-transformer-prismic for use with gatsby-image.
         // The document node, field key (i.e. API ID), and field value are
@@ -136,9 +132,12 @@ module.exports = {
         // This defaults to always return true.
         shouldNormalizeImage: ({ node, key, value }) => {
           // Return true to normalize the image or false to skip.
-          return true
+          return true;
+        },
+        schemas: {
+          page: require("./src/schemas/news_story.json"),
         },
       },
     },
   ],
-}
+};
